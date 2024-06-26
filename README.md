edbbb95ff471338a4d0f7ba8708ed36af50bec0b
garrow <garik.steam.11@yandex.ru>
2024-06-26 08:19:29 -0700

    Add Different Procedures

--- README.md
+++ README.md
@@ -36,3 +36,4 @@
 - Flyway
 - Docker
 - Swagger
+- Maven
---
+++ src/main/java/com/garrow/coffeemachine/procedures/CoffeeExtractionProcedure.java
@@ -0,0 +1,19 @@
+package com.garrow.coffeemachine.procedures;
+
+import com.garrow.coffeemachine.models.ActionArgument;
+import com.garrow.coffeemachine.models.ActionIngredient;
+import com.garrow.coffeemachine.procedures.interfaces.Procedure;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+public class CoffeeExtractionProcedure implements Procedure {
+
+    @Override
+    public void execute(List<ActionIngredient> actionIngredients, List<ActionArgument> actionArguments) {
+        // Какая-то реальная логика по процедуре, которая обращается к реальной кофемашине
+        log.info("CoffeeExtractionProcedure started with actionIngredients: {} and actionArguments: {}", 1, 1);
+    }
+
+}
